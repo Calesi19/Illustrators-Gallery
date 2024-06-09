363a00d993bc36c22b8c5f7be96fd57c7ab7e3a3
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-06-08 22:19:20 -0500

    fix: compresses images

--- src/app/Gallery.tsx
+++ src/app/Gallery.tsx
@@ -7,82 +7,82 @@ const imagesArray = [
   {
     alt: "Image1's alt text",
     caption: "Whale in the ocean",
-    src: "/artwork/16.png",
+    src: "/artwork/16.webp",
   },
   {
     alt: "Image1's alt text",
     caption: "Aztec Woman",
-    src: "/artwork/1.png",
+    src: "/artwork/1.webp",
   },
   {
     alt: "Image2's alt text",
     caption: "Alfred the Crocodile",
-    src: "/artwork/2.png",
+    src: "/artwork/2.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "Underwater Turtle",
-    src: "/artwork/15.png",
+    src: "/artwork/15.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "Simple Man",
-    src: "/artwork/3.png",
+    src: "/artwork/3.webp",
   },
   {
     alt: "Image1's alt text",
     caption: "Red,Red,Red",
-    src: "/artwork/6.png",
+    src: "/artwork/6.webp",
   },
   {
     alt: "Image2's alt text",
     caption: "Fish on a Pirate Ship",
-    src: "/artwork/5.png",
+    src: "/artwork/5.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "Custom Harry Potter Cover",
-    src: "/artwork/4.png",
+    src: "/artwork/4.webp",
   },
   {
     alt: "Image2's alt text",
     caption: "Balance",
-    src: "/artwork/7.png",
+    src: "/artwork/7.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "Color Matching a Bottle",
-    src: "/artwork/8.png",
+    src: "/artwork/8.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "The Majestic Condor",
-    src: "/artwork/14.png",
+    src: "/artwork/14.webp",
   },
   {
     alt: "Image2's alt text",
     caption: "Man in a Cave",
-    src: "/artwork/9.png",
+    src: "/artwork/9.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "Whale Poster",
-    src: "/artwork/13.png",
+    src: "/artwork/13.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "The Creation of Adam",
-    src: "/artwork/10.png",
+    src: "/artwork/10.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "Windows to Art",
-    src: "/artwork/11.png",
+    src: "/artwork/11.webp",
   },
   {
     alt: "Image3's alt text",
     caption: "Peace Poster",
-    src: "/artwork/12.png",
+    src: "/artwork/12.webp",
   },
 ];
 
