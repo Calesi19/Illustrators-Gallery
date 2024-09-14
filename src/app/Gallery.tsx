768473e45f8f71fd24ccd2c9dfa5cc69aeae4c94
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-09-14 18:58:16 -0500

    fix: alt text

--- src/app/Gallery.tsx
+++ src/app/Gallery.tsx
@@ -1,86 +1,86 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { ImageGallery } from 'react-image-grid-gallery';
+import React from "react";
+import { ImageGallery } from "react-image-grid-gallery";
 
 const imagesArray = [
   {
-    alt: "Image1's alt text",
+    alt: "Whale in the ocean",
     caption: "Whale in the ocean",
     src: "/artwork/16.webp",
   },
   {
-    alt: "Image1's alt text",
+    alt: "Aztec Woman",
     caption: "Aztec Woman",
     src: "/artwork/1.webp",
   },
   {
-    alt: "Image2's alt text",
+    alt: "Alfred the Crocodile",
     caption: "Alfred the Crocodile",
     src: "/artwork/2.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "Underwater Turtle",
     caption: "Underwater Turtle",
     src: "/artwork/15.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "Simple Man",
     caption: "Simple Man",
     src: "/artwork/3.webp",
   },
   {
-    alt: "Image1's alt text",
+    alt: "Red,Red,Red",
     caption: "Red,Red,Red",
     src: "/artwork/6.webp",
   },
   {
-    alt: "Image2's alt text",
+    alt: "Fish on a Pirate Ship",
     caption: "Fish on a Pirate Ship",
     src: "/artwork/5.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "Custom Harry Potter Cover",
     caption: "Custom Harry Potter Cover",
     src: "/artwork/4.webp",
   },
   {
-    alt: "Image2's alt text",
+    alt: "Balance",
     caption: "Balance",
     src: "/artwork/7.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "Color Matching a Bottle",
     caption: "Color Matching a Bottle",
     src: "/artwork/8.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "The Majestic Condor",
     caption: "The Majestic Condor",
     src: "/artwork/14.webp",
   },
   {
-    alt: "Image2's alt text",
+    alt: "Man in a Cave",
     caption: "Man in a Cave",
     src: "/artwork/9.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "Whale Poster",
     caption: "Whale Poster",
     src: "/artwork/13.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "The Creation of Adam",
     caption: "The Creation of Adam",
     src: "/artwork/10.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "Windows to Art",
     caption: "Windows to Art",
     src: "/artwork/11.webp",
   },
   {
-    alt: "Image3's alt text",
+    alt: "Peace Poster",
     caption: "Peace Poster",
     src: "/artwork/12.webp",
   },
