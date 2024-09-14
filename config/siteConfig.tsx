1be749db348982428b56355b7e1d72803db92484
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-09-14 18:56:24 -0500

    fix: typo bio

--- config/siteConfig.tsx
+++ config/siteConfig.tsx
@@ -6,7 +6,7 @@ export const siteConfig = {
   title: "illustration",
   about_headline: "Creating Children's Illustrations",
   about_body:
-    "I am a freelance illustrator based in Minnesota. I specialize in paint and digital illustration's for children's books. I am currently available for freelance work.",
+    "I am a freelance illustrator based in Minnesota. I specialize in paint and digital illustrations for children's books. I am currently available for freelance work.",
   email: "jayme0311@gmail.com",
   phone: "612-242-0890",
   linkedin: "https://www.linkedin.com/in/jayme-m-b5148a25a/",
