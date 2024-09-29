a0c9602153ea549953dc1b5abe8dc6e9cc6ecd7a
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-09-28 19:56:51 -0500

    remove

--- src/app/Gallery.tsx
+++ src/app/Gallery.tsx
@@ -10,34 +10,9 @@ const imagesArray = [
     src: "/artwork/16.webp",
   },
   {
-    alt: "Aztec Woman",
-    caption: "Aztec Woman",
-    src: "/artwork/1.webp",
-  },
-  {
-    alt: "Alfred the Crocodile",
-    caption: "Alfred the Crocodile",
-    src: "/artwork/2.webp",
-  },
-  {
     alt: "Underwater Turtle",
     caption: "Underwater Turtle",
     src: "/artwork/15.webp",
-  },
-  {
-    alt: "Simple Man",
-    caption: "Simple Man",
-    src: "/artwork/3.webp",
-  },
-  {
-    alt: "Red,Red,Red",
-    caption: "Red,Red,Red",
-    src: "/artwork/6.webp",
-  },
-  {
-    alt: "Fish on a Pirate Ship",
-    caption: "Fish on a Pirate Ship",
-    src: "/artwork/5.webp",
   },
   {
     alt: "Custom Harry Potter Cover",
@@ -50,30 +25,11 @@ const imagesArray = [
     src: "/artwork/7.webp",
   },
   {
-    alt: "Color Matching a Bottle",
-    caption: "Color Matching a Bottle",
-    src: "/artwork/8.webp",
-  },
-  {
     alt: "The Majestic Condor",
     caption: "The Majestic Condor",
     src: "/artwork/14.webp",
   },
-  {
-    alt: "Man in a Cave",
-    caption: "Man in a Cave",
-    src: "/artwork/9.webp",
-  },
-  {
-    alt: "Whale Poster",
-    caption: "Whale Poster",
-    src: "/artwork/13.webp",
-  },
-  {
-    alt: "The Creation of Adam",
-    caption: "The Creation of Adam",
-    src: "/artwork/10.webp",
-  },
+
   {
     alt: "Windows to Art",
     caption: "Windows to Art",
